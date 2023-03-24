e2068731934dd5ece66928eb6886c2984f43867e
Paolo Campus <56634925+PaoloCMP@users.noreply.github.com>
2023-03-24 22:45:23 +0100

    Add files via upload

---
+++ scripts/disabler.js
@@ -0,0 +1,22 @@
+function ambientModeTurnOff () {
+    
+    const ambientModeSwitch = document.getElementsByClassName('ytp-menuitem')[0]
+
+    if( (ambientModeSwitch.hasAttribute('aria-checked')) && (ambientModeSwitch.getAttribute('aria-checked') === "true") ){      
+            ambientModeSwitch.click()
+            setTimeout(ambientModeTurnOff, 100)
+        } 
+    }
+
+function main() {
+
+    if (window.location.pathname !== "/watch"){
+         return
+    }
+
+    document.getElementsByClassName('ytp-settings-button')[0].click()
+    setTimeout(ambientModeTurnOff, 100)
+}
+
+(document || window).addEventListener("yt-navigate-finish", main, true);
+
